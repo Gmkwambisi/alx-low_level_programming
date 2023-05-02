e706c1f2a779a2ebe548cabed0f7effa30751e42
gmkwambisi <gmkwambisi@gmail.com>
2023-05-02 08:12:00 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,21 @@
 #include "lists.h"
-/**
- * listint_len - Returns the number of elements in a list
- * @h: the list to check
- * Return: the number of elements in the list
- */
 
-size_t listint_len(const listint_t *h)
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
 {
-	size_t counter = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		counter++;
+		printf("%d\n", h->n);
+		num++;
 		h = h->next;
 	}
-	return (counter);
+
+	return (num);
 }
